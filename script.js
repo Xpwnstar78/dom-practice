6f534473c5b09da9960ff3f05d205e50105cc98f
Xpwnstar78 <xpwnstar78@gmail.com>
2020-10-23 19:35:05 -0400

    initial commit

--- script.js
+++ script.js
@@ -20,16 +20,37 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  let title = document.getElementById("main-title");
+  title.innerHTML = "My name is DOM!";
   // Part 2
-
+  document.body.style.backgroundColor = "lightgreen";
   // Part 3
-
+  let menu = document.getElementById('favorite-things');
+menu.removeChild(menu.lastElementChild);
   // Part 4
+  let elements = document.getElementsByClassName('special-title');
 
+for (let i = 0; i < elements.length; i++) {
+  let element = elements[i];
+  element.style.fontSize = "2rem";
+}
   // Part 5
-
+  let list = document.getElementById("past-races");
+   list.removeChild(list.childNodes[7]);
   // Part 6
-
+  let node = document.createElement("LI");                 
+  let textnode = document.createTextNode("Brooklyn");         
+  node.appendChild(textnode);                              
+  document.getElementById("past-races").appendChild(node);     
   // Part 7
+  let div = document.createElement('div');
+  div.id = 'dom-adventures';
+div.className = 'special-title';  
+div.className = 'blog-post purple';
+div.innerHTML = '<h1>Brooklyn</h1>';
+let text = document.createTextNode('I ran over everyone!');
+  div.appendChild(text);
+  div.style.textAlign = "center";
+  div.style.background = "purple";
+document.body.appendChild(div);
 }
